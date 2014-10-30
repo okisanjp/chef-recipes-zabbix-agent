da7f3aeaa8ee70b45ddebc4061e5240d62624969
okisanjp <okisan.jp@gmail.com>
2014-10-30 14:31:30 +0900

    .

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
-name             'zabbix-agent'
+name             'chef-recipes-zabbix-agent'
 maintainer       'YOUR_COMPANY_NAME'
 maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
-description      'Installs/Configures ops-tools'
+description      'Installs/Configures chef-recipes-zabbix-agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
